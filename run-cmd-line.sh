c244058d091d3f9858d40eb8e8f8e01ba662974e
whoana <whoana@gmail.com>
2022-06-30 15:21:40 +0900

    office

--- run-cmd-line.sh
+++ run-cmd-line.sh
@@ -1,1 +1,5 @@
- java -cp ./home/lib/t9-trace-1.0.0.jar -Drose.mary.home=/Users/whoana/DEV/workspace-vs/t9/home -Dloader.main=rose.mary.trace.T9 --add-opens jdk.management/com.sun.management.internal=ALL-UNNAMED org.springframework.boot.loader.T9Launcher
+ java -cp ./t9-install-linux/lib/t9-trace-1.0.0.jar \
+      -Drose.mary.home=/Users/whoana/DEV/workspace-vs/t9/home \
+      -Dloader.main=rose.mary.trace.T9 \
+      --add-opens jdk.management/com.sun.management.internal=ALL-UNNAMED \
+      org.springframework.boot.loader.T9Launcher
--- t9-trace/src/main/java/rose/mary/trace/T9.java
+++ t9-trace/src/main/java/rose/mary/trace/T9.java
@@ -16,6 +16,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.ApplicationPidFileWriter;
+
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.MessageSource;
@@ -39,11 +40,11 @@ import rose.mary.trace.manager.InterfaceCacheManager;
 import rose.mary.trace.manager.ServerManager;
 import rose.mary.trace.system.SystemLogger;
 
-
 /**
  * <pre>
  * The Application is a spring boot main application
  * </pre>
+ * 
  * @author whoana
  * @since Aug 7, 2019
  */
@@ -51,39 +52,37 @@ import rose.mary.trace.system.SystemLogger;
 @RestController
 @SpringBootApplication
 @ImportResource("classpath:trace-context.xml")
-@ComponentScan({"rose.mary.trace"})
-public class T9 implements CommandLineRunner, ApplicationListener<ContextClosedEvent>, InitializingBean, DisposableBean {
-	
-	
+@ComponentScan({ "rose.mary.trace" })
+public class T9
+		implements CommandLineRunner, ApplicationListener<ContextClosedEvent>, InitializingBean, DisposableBean {
+
 	@Autowired
 	MessageSource messageSource;
-	
+
 	@Autowired
 	SystemResourceMonitor srm;
-	 
-	
+
 	@Autowired
 	DatabasePolicyHandlerManager databasePolicyHandlerManager;
 
 	@Autowired
 	ServerManager serverManager;
-	
+
 	@Autowired
 	CacheManager cacheManager;
-	
+
 	@Autowired
 	RuntimeInfo runtimeInfo;
-	
-	@Autowired 
+
+	@Autowired
 	InterfaceCacheManager interfaceCacheManager;
-	
+
 	@Autowired
 	ConfigurationManager configurationManager;
-	 
+
 	@Autowired
 	DataSource dataSource;
-	
-	
+
 	@Override
 	public void destroy() throws Exception {
 		SystemLogger.info("good-bye t9.");
@@ -91,14 +90,14 @@ public class T9 implements CommandLineRunner, ApplicationListener<ContextClosedE
 
 	@Override
 	public void afterPropertiesSet() throws Exception {
-		
+
 	}
 
 	@Override
 	public void onApplicationEvent(ContextClosedEvent event) {
-		
+
 		try {
-			//traceServer.stop();
+			// traceServer.stop();
 			serverManager.stopServer();
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -106,10 +105,12 @@ public class T9 implements CommandLineRunner, ApplicationListener<ContextClosedE
 		cacheManager.closeCache();
 		sayEnding();
 	}
- 
+
 	@Override
 	public void run(String... args) throws Exception {
 		
+		
+
 		SystemLogger.info("args:" + Util.toJSONString(args));
 		
 		isRecoveryMode = System.getProperty("rose.mary.run.mode", "server").equals("recovery") ? true : false;
@@ -126,36 +127,36 @@ public class T9 implements CommandLineRunner, ApplicationListener<ContextClosedE
 		afterBoot();		 
 	}
 
-	
-	
-	
 	/**
 	 * 
 	 */
 	private void afterBoot() throws Exception {
-		if(isRecoveryMode) {
+		if (isRecoveryMode) {
 			try {
 				SystemLogger.info("recovery mode starting");
-				//ToDo: not implemented yet
-				//recoveryHandler.start();
-			}finally { 
-				
+				// ToDo: not implemented yet
+				// recoveryHandler.start();
+			} finally {
+
 			}
-			
-		}else {
-			// if(configurationManager.getServerManagerConfig().isStartOnBoot()) traceServer.start();
-			if(configurationManager.getServerManagerConfig().isStartOnBoot()) serverManager.startServer();
-			else SystemLogger.info("getServerManagerConfig().isStartOnBoot():false");
-			//traceServer.startDatabasePolicyHandler();
+
+		} else {
+			// if(configurationManager.getServerManagerConfig().isStartOnBoot())
+			// traceServer.start();
+			if (configurationManager.getServerManagerConfig().isStartOnBoot())
+				serverManager.startServer();
+			else
+				SystemLogger.info("getServerManagerConfig().isStartOnBoot():false");
+			// traceServer.startDatabasePolicyHandler();
 			databasePolicyHandlerManager.start();
 		}
 	}
-	
+
 	static ConfigurableApplicationContext ctx;
-	
+
 	static boolean isRecoveryMode = false;
-	
-	public static void main(String[] args) { 
+
+	public static void main(String[] args) {
 		SpringApplicationBuilder sab = new SpringApplicationBuilder(T9.class);
 		{
 			String iwannadie = System.getProperty("rose.mary.home", ".") + File.separator + "iwannadie.pid";
@@ -163,123 +164,159 @@ public class T9 implements CommandLineRunner, ApplicationListener<ContextClosedE
 		}
 		ctx = sab.run(args);
 	}
-	 
-	
+
 	public static void exitApplication() {
 		int exitCode = 0;
 		try {
 			exitCode = SpringApplication.exit(ctx, new ExitCodeGenerator() {
-			@Override
-			public int getExitCode() {
-				// no errors
-				return 0;
-			}
-		});
-		}catch(Throwable t) {
-			//SystemLogger.error("boot shutdown error",t);
+				@Override
+				public int getExitCode() {
+					// no errors
+					return 0;
+				}
+			});
+		} catch (Throwable t) {
+			// SystemLogger.error("boot shutdown error",t);
 		}
 		System.exit(exitCode);
 	}
 
- 
-	
-	 
-	private void sayStartingMsg() {		 
+	private void sayStartingMsg() {
 		StringBuffer msg = new StringBuffer();
 		msg.append(System.lineSeparator());
 		msg.append(SystemLogger.astars(80));
-		msg.append(SystemLogger.astar).append(" " + messageSource.getMessage("system.start.msg", null, "The server t9", null)).append(" starting").append(System.lineSeparator());
-		msg.append(SystemLogger.astar).append(" date    :").append(Util.getFormatedDate()).append(System.lineSeparator());
-		msg.append(SystemLogger.astar).append(" version :").append(messageSource.getMessage("system.version", null, "1.0.0.", null)).append(System.lineSeparator());	
-		msg.append(SystemLogger.astar).append(" bootKey :").append(runtimeInfo.getBootKey()).append(System.lineSeparator());
-		msg.append(SystemLogger.astar).append(" home    :").append(System.getProperty("rose.mary.home", ".")).append(System.lineSeparator());
-		msg.append(SystemLogger.astar).append(" mode    :").append(System.getProperty("rose.mary.run.mode", "server")).append(System.lineSeparator());
-		
-		
+		msg.append(SystemLogger.astar)
+				.append(" " + messageSource.getMessage("system.start.msg", null, "The server t9", null))
+				.append(" starting").append(System.lineSeparator());
+		msg.append(SystemLogger.astar).append(" date    :").append(Util.getFormatedDate())
+				.append(System.lineSeparator());
+		msg.append(SystemLogger.astar).append(" version :")
+				.append(messageSource.getMessage("system.version", null, "1.0.0.", null))
+				.append(System.lineSeparator());
+		msg.append(SystemLogger.astar).append(" bootKey :").append(runtimeInfo.getBootKey())
+				.append(System.lineSeparator());
+		msg.append(SystemLogger.astar).append(" home    :").append(System.getProperty("rose.mary.home", "."))
+				.append(System.lineSeparator());
+		msg.append(SystemLogger.astar).append(" mode    :").append(System.getProperty("rose.mary.run.mode", "server"))
+				.append(System.lineSeparator());
+
 		msg.append(getSystemResource());
-		
-		msg.append(SystemLogger.astars(80));		
-		SystemLogger.info(msg.toString());	 
-		 
+
+		msg.append(SystemLogger.astars(80));
+		SystemLogger.info(msg.toString());
+
 	}
 
 	private StringBuffer getSystemResource() {
 		StringBuffer msg = new StringBuffer();
 		SystemResource res = srm.watch();
-		/*		
+		/*
+		 * msg.append(SystemLogger.astar).append(" system resource :").append(System.
+		 * lineSeparator());
+		 * msg.append(SystemLogger.astar).append("\tjavaVersion  :").append(res.
+		 * getJavaVersion()).append(System.lineSeparator());
+		 * msg.append(SystemLogger.astar).append("\tjavaVendor   :").append(res.
+		 * getJavaVendor()).append(System.lineSeparator());
+		 * msg.append(SystemLogger.astar).append("\tjavaHome     :").append(res.
+		 * getJavaHome()).append(System.lineSeparator());
+		 * msg.append(SystemLogger.astar).append("\tjavaClassVersion:").append(res.
+		 * getJavaClassVersion()).append(System.lineSeparator());
+		 * //msg.append(SystemLog.astar).append("\tjavaClassPath:").append(res.
+		 * getJavaClassPath()).append(System.lineSeparator());
+		 * msg.append(SystemLogger.astar).append("\tosName       :").append(res.
+		 * getOsName()).append(System.lineSeparator());
+		 * msg.append(SystemLogger.astar).append("\tosArch       :").append(res.
+		 * getOsArch()).append(System.lineSeparator());
+		 * msg.append(SystemLogger.astar).append("\tosVersion    :").append(res.
+		 * getOsVersion()).append(System.lineSeparator());
+		 * msg.append(SystemLogger.astar).append("\tuserName     :").append(res.
+		 * getUserName()).append(System.lineSeparator());
+		 * msg.append(SystemLogger.astar).append("\tuserHome     :").append(res.
+		 * getUserHome()).append(System.lineSeparator());
+		 * msg.append(SystemLogger.astar).append("\tuserDir      :").append(res.
+		 * getUserDir()).append(System.lineSeparator());
+		 * msg.append(SystemLogger.astar).append("\ttotalDiskAmt :").append(res.
+		 * getTotalDiskAmt()).append(System.lineSeparator());
+		 * msg.append(SystemLogger.astar).append("\tusedDiskAmt  :").append(res.
+		 * getUsedDiskAmt()).append(System.lineSeparator());
+		 * msg.append(SystemLogger.astar).append("\tidleDiskAmt  :").append(res.
+		 * getIdleDiskAmt()).append(System.lineSeparator());
+		 * msg.append(SystemLogger.astar).append("\tusedDiskPct  :").append(res.
+		 * getUsedDiskPct()).append(System.lineSeparator());
+		 * msg.append(SystemLogger.astar).append("\tidleDiskPct  :").append(res.
+		 * getIdleDiskPct()).append(System.lineSeparator());
+		 * msg.append(SystemLogger.astar).append("\tusedCpuPct   :").append(res.
+		 * getUsedCpuPct()).append(System.lineSeparator());
+		 * msg.append(SystemLogger.astar).append("\tidleCpuPct   :").append(res.
+		 * getIdleCpuPct()).append(System.lineSeparator());
+		 * msg.append(SystemLogger.astar).append("\ttotalMemAmt  :").append(res.
+		 * getTotalMemAmt()).append(System.lineSeparator());
+		 * msg.append(SystemLogger.astar).append("\tidleMemAmt   :").append(res.
+		 * getIdleMemAmt()).append(System.lineSeparator());
+		 * msg.append(SystemLogger.astar).append("\tusedMemPct   :").append(res.
+		 * getUsedMemPct()).append(System.lineSeparator());
+		 * msg.append(SystemLogger.astar).append("\tidleMemPct   :").append(res.
+		 * getIdleMemPct()).append(System.lineSeparator());
+		 */
 		msg.append(SystemLogger.astar).append(" system resource :").append(System.lineSeparator());
-		msg.append(SystemLogger.astar).append("\tjavaVersion  :").append(res.getJavaVersion()).append(System.lineSeparator());		
-		msg.append(SystemLogger.astar).append("\tjavaVendor   :").append(res.getJavaVendor()).append(System.lineSeparator());
-		msg.append(SystemLogger.astar).append("\tjavaHome     :").append(res.getJavaHome()).append(System.lineSeparator());
-		msg.append(SystemLogger.astar).append("\tjavaClassVersion:").append(res.getJavaClassVersion()).append(System.lineSeparator());
-		//msg.append(SystemLog.astar).append("\tjavaClassPath:").append(res.getJavaClassPath()).append(System.lineSeparator());		
-		msg.append(SystemLogger.astar).append("\tosName       :").append(res.getOsName()).append(System.lineSeparator());
-		msg.append(SystemLogger.astar).append("\tosArch       :").append(res.getOsArch()).append(System.lineSeparator());
-		msg.append(SystemLogger.astar).append("\tosVersion    :").append(res.getOsVersion()).append(System.lineSeparator());
-		msg.append(SystemLogger.astar).append("\tuserName     :").append(res.getUserName()).append(System.lineSeparator());
-		msg.append(SystemLogger.astar).append("\tuserHome     :").append(res.getUserHome()).append(System.lineSeparator());
-		msg.append(SystemLogger.astar).append("\tuserDir      :").append(res.getUserDir()).append(System.lineSeparator());
-		msg.append(SystemLogger.astar).append("\ttotalDiskAmt :").append(res.getTotalDiskAmt()).append(System.lineSeparator());		
-		msg.append(SystemLogger.astar).append("\tusedDiskAmt  :").append(res.getUsedDiskAmt()).append(System.lineSeparator());
-		msg.append(SystemLogger.astar).append("\tidleDiskAmt  :").append(res.getIdleDiskAmt()).append(System.lineSeparator());
-		msg.append(SystemLogger.astar).append("\tusedDiskPct  :").append(res.getUsedDiskPct()).append(System.lineSeparator());
-		msg.append(SystemLogger.astar).append("\tidleDiskPct  :").append(res.getIdleDiskPct()).append(System.lineSeparator()); 
-		msg.append(SystemLogger.astar).append("\tusedCpuPct   :").append(res.getUsedCpuPct()).append(System.lineSeparator());
-		msg.append(SystemLogger.astar).append("\tidleCpuPct   :").append(res.getIdleCpuPct()).append(System.lineSeparator());
-		msg.append(SystemLogger.astar).append("\ttotalMemAmt  :").append(res.getTotalMemAmt()).append(System.lineSeparator());
-		msg.append(SystemLogger.astar).append("\tidleMemAmt   :").append(res.getIdleMemAmt()).append(System.lineSeparator());
-		msg.append(SystemLogger.astar).append("\tusedMemPct   :").append(res.getUsedMemPct()).append(System.lineSeparator());
-		msg.append(SystemLogger.astar).append("\tidleMemPct   :").append(res.getIdleMemPct()).append(System.lineSeparator());
-		*/
-		msg.append(SystemLogger.astar).append(" system resource :").append(System.lineSeparator());
-		msg.append(SystemLogger.astar).append("\tA01 : ").append(res.getJavaVersion()).append(System.lineSeparator());		
+		msg.append(SystemLogger.astar).append("\tA01 : ").append(res.getJavaVersion()).append(System.lineSeparator());
 		msg.append(SystemLogger.astar).append("\tA02 : ").append(res.getJavaVendor()).append(System.lineSeparator());
 		msg.append(SystemLogger.astar).append("\tA03 : ").append(res.getJavaHome()).append(System.lineSeparator());
-		msg.append(SystemLogger.astar).append("\tA04 : ").append(res.getJavaClassVersion()).append(System.lineSeparator());
-		//msg.append(SystemLog.astar).append("\tA06 : ").append(res.getJavaClassPath()).append(System.lineSeparator());		
+		msg.append(SystemLogger.astar).append("\tA04 : ").append(res.getJavaClassVersion())
+				.append(System.lineSeparator());
+		// msg.append(SystemLog.astar).append("\tA06 :
+		// ").append(res.getJavaClassPath()).append(System.lineSeparator());
 		msg.append(SystemLogger.astar).append("\tA07 : ").append(res.getOsName()).append(System.lineSeparator());
 		msg.append(SystemLogger.astar).append("\tA08 : ").append(res.getOsArch()).append(System.lineSeparator());
 		msg.append(SystemLogger.astar).append("\tA09 : ").append(res.getOsVersion()).append(System.lineSeparator());
 		msg.append(SystemLogger.astar).append("\tA10 : ").append(res.getUserName()).append(System.lineSeparator());
 		msg.append(SystemLogger.astar).append("\tA11 : ").append(res.getUserHome()).append(System.lineSeparator());
 		msg.append(SystemLogger.astar).append("\tA12 : ").append(res.getUserDir()).append(System.lineSeparator());
-		msg.append(SystemLogger.astar).append("\tA13 : ").append(res.getTotalDiskAmt()).append(System.lineSeparator());		
+		msg.append(SystemLogger.astar).append("\tA13 : ").append(res.getTotalDiskAmt()).append(System.lineSeparator());
 		msg.append(SystemLogger.astar).append("\tA14 : ").append(res.getUsedDiskAmt()).append(System.lineSeparator());
 		msg.append(SystemLogger.astar).append("\tA15 : ").append(res.getIdleDiskAmt()).append(System.lineSeparator());
 		msg.append(SystemLogger.astar).append("\tA16 : ").append(res.getUsedDiskPct()).append(System.lineSeparator());
-		msg.append(SystemLogger.astar).append("\tA17 : ").append(res.getIdleDiskPct()).append(System.lineSeparator()); 
+		msg.append(SystemLogger.astar).append("\tA17 : ").append(res.getIdleDiskPct()).append(System.lineSeparator());
 		msg.append(SystemLogger.astar).append("\tA18 : ").append(res.getUsedCpuPct()).append(System.lineSeparator());
 		msg.append(SystemLogger.astar).append("\tA19 : ").append(res.getIdleCpuPct()).append(System.lineSeparator());
 		msg.append(SystemLogger.astar).append("\tA20 : ").append(res.getTotalMemAmt()).append(System.lineSeparator());
 		msg.append(SystemLogger.astar).append("\tA21 : ").append(res.getIdleMemAmt()).append(System.lineSeparator());
 		msg.append(SystemLogger.astar).append("\tA22 : ").append(res.getUsedMemPct()).append(System.lineSeparator());
 		msg.append(SystemLogger.astar).append("\tA23 : ").append(res.getIdleMemPct()).append(System.lineSeparator());
-		
+
 		return msg;
 	}
-	
+
 	private void sayEnding() {
 		StringBuffer msg = new StringBuffer();
 		msg.append(System.lineSeparator());
 		msg.append(SystemLogger.astars(80));
-		msg.append(SystemLogger.astar).append(" " + messageSource.getMessage("system.start.msg", null, "The server t9", null)).append(" ending").append(System.lineSeparator());
-		msg.append(SystemLogger.astar).append(" date    :").append(Util.getFormatedDate()).append(System.lineSeparator());
-		msg.append(SystemLogger.astar).append(" version :").append(messageSource.getMessage("system.version", null, "1.0.0.", null)).append(System.lineSeparator());	
-		msg.append(SystemLogger.astar).append(" bootKey :").append(runtimeInfo.getBootKey()).append(System.lineSeparator());
-		msg.append(SystemLogger.astar).append(" home    :").append(System.getProperty("rose.mary.home", ".")).append(System.lineSeparator());
-		msg.append(SystemLogger.astar).append(" mode    :").append(System.getProperty("rose.mary.run.mode", "server")).append(System.lineSeparator());
-		msg.append(SystemLogger.astar).append(" running :").append(((System.currentTimeMillis() - runtimeInfo.getStartedTime())/1000/60) + "M").append(System.lineSeparator());
-		
+		msg.append(SystemLogger.astar)
+				.append(" " + messageSource.getMessage("system.start.msg", null, "The server t9", null))
+				.append(" ending").append(System.lineSeparator());
+		msg.append(SystemLogger.astar).append(" date    :").append(Util.getFormatedDate())
+				.append(System.lineSeparator());
+		msg.append(SystemLogger.astar).append(" version :")
+				.append(messageSource.getMessage("system.version", null, "1.0.0.", null))
+				.append(System.lineSeparator());
+		msg.append(SystemLogger.astar).append(" bootKey :").append(runtimeInfo.getBootKey())
+				.append(System.lineSeparator());
+		msg.append(SystemLogger.astar).append(" home    :").append(System.getProperty("rose.mary.home", "."))
+				.append(System.lineSeparator());
+		msg.append(SystemLogger.astar).append(" mode    :").append(System.getProperty("rose.mary.run.mode", "server"))
+				.append(System.lineSeparator());
+		msg.append(SystemLogger.astar).append(" running :")
+				.append(((System.currentTimeMillis() - runtimeInfo.getStartedTime()) / 1000 / 60) + "M")
+				.append(System.lineSeparator());
+
 		msg.append(getSystemResource());
-		
+
 		msg.append(SystemLogger.astars(80));
-		if(serverManager.getWarnning() != null) {
+		if (serverManager.getWarnning() != null) {
 			msg.append(serverManager.getWarnning());
 			msg.append(SystemLogger.astars(80));
 		}
 		SystemLogger.info(msg.toString());
 	}
-	
- 
-	
+
 }
