cb27ad806b3041d5181ab07c97fc189011201fe4
whoana <whoana@gmail.com>
2022-07-18 14:29:07 +0900

    Highlighter

--- t9-trace/src 복사본/test/java/rose/mary/trace/sample/LambdaDemo.java
+++
@@ -1,9 +0,0 @@
-package rose.mary.trace.sample;
-
-public class LambdaDemo
-{
-   public static void main(String[] args)
-   {
-      new Thread(() -> {System.out.println("Hello"); System.out.println("Hello");}).start();
-   }
-}
