092f50ff3a32879d1df3c406b680675f1dbb31e2
whoana <whoana@gmail.com>
2022-09-21 08:44:16 +0900

    테스트 메시지 생성 옵션 추가 - integrationId

--- t9-trace/src/main/java/rose/mary/trace/service/GenerateTraceMsgService.java
+++ t9-trace/src/main/java/rose/mary/trace/service/GenerateTraceMsgService.java
@@ -2,7 +2,7 @@
  * Copyright 2020 portal.mocomsys.com All Rights Reserved.
  */
 package rose.mary.trace.service;
- 
+
 import java.util.Map;
 
 import org.apache.commons.lang3.time.StopWatch;
@@ -20,6 +20,7 @@ import rose.mary.trace.manager.CacheManager;
  * rose.mary.trace.service.test
  * GenerateTraceMsgService.java
  * </pre>
+ * 
  * @author whoana
  * @date Jul 26, 2019
  */
@@ -27,29 +28,27 @@ import rose.mary.trace.manager.CacheManager;
 public class GenerateTraceMsgService {
 
 	Logger logger = LoggerFactory.getLogger(GenerateTraceMsgService.class);
-	
-	
+
 	@Autowired
 	CacheManager cacheManager;
-	 
-	
-	public String generate(Map<String, String> params) throws Throwable{
+
+	public String generate(Map<String, String> params) throws Throwable {
 		StringBuffer res = new StringBuffer();
 		try {
-			int port;					//-po
-			String hostname; 			//-ho
-			String qmgrName; 			//-qm
-			String userId; 				//-um
-			String password; 			//-ps
-			String channelName; 		//-ch
-			String queueName; 			//-qu
-			String module;				//-mo
-			int generateCount = 1000; 	//-ge
-			int commitCount = 100;		//-co
-			String data;				//-da
-			String traceMsgCreator;     //-tr
+			int port; // -po
+			String hostname; // -ho
+			String qmgrName; // -qm
+			String userId; // -um
+			String password; // -ps
+			String channelName; // -ch
+			String queueName; // -qu
+			String module; // -mo
+			int generateCount = 1000; // -ge
+			int commitCount = 100; // -co
+			String data; // -da
+			String traceMsgCreator; // -tr
 			TraceMsgCreator tmc = null;
-	
+
 			if (checkParams(params)) {
 				qmgrName = params.get("qmgrName");
 				userId = params.get("userId");
@@ -64,53 +63,53 @@ public class GenerateTraceMsgService {
 				} catch (NumberFormatException e) {
 					port = 1414;
 				}
-				 
+
 				try {
 					generateCount = Integer.parseInt(params.get("generateCount"));
 				} catch (NumberFormatException e) {
 					generateCount = 1000;
 				}
-				
+
 				try {
 					commitCount = Integer.parseInt(params.get("commitCount"));
 				} catch (NumberFormatException e) {
 					commitCount = 100;
 				}
-				
+
 				traceMsgCreator = params.get("traceMsgCreator");
-				if(traceMsgCreator != null) {
+				if (traceMsgCreator != null) {
 					Class clazz = Class.forName(traceMsgCreator);
-					tmc = (TraceMsgCreator)clazz.newInstance(); 
+					tmc = (TraceMsgCreator) clazz.newInstance();
 				}
-				
-			} else { 
+
+			} else {
 				throw new IllegalArgumentException();
-			}	
-			 
-			
-			TraceMsgGenerator tmg = new TraceMsgGenerator(hostname, port, qmgrName, userId, password, channelName, queueName, module, generateCount, commitCount, data, tmc, cacheManager.getInterfaceCache());
-			
+			}
+
+			TraceMsgGenerator tmg = new TraceMsgGenerator(hostname, port, qmgrName, userId, password, channelName,
+					queueName, module, generateCount, commitCount, data, tmc, cacheManager.getInterfaceCache());
+
 			logger.info("------------------------------------------");
 			logger.info("- start task information");
 			logger.info("------------------------------------------");
 			logger.info(tmg.information());
 			logger.info("------------------------------------------");
 			logger.info("- initailizing . . .");
-			
+
 			tmg.initialize();
-			
+
 			logger.info("- generating msgs");
 			logger.info("------------------------------------------");
 			StopWatch sw = new StopWatch();
 			sw.start();
-			tmg.generate();
+			tmg.generate(params.get("integrationId"));
 			sw.stop();
 			logger.info("------------------------------------------");
 			logger.info("- generated msg successly.(elapsed:" + sw.getTime() + " ms)");
 			logger.info("- end task");
 			logger.info("------------------------------------------");
 			return res.append(tmg.information()).toString();
-		}catch (Throwable t) {
+		} catch (Throwable t) {
 			logger.info("------------------------------------------");
 			logger.info("- fail to generatd msgs.");
 			logger.info("------------------------------------------");
@@ -118,27 +117,24 @@ public class GenerateTraceMsgService {
 			throw t;
 		}
 	}
-	
 
-
-
-	public String generateNodeMsg(Map<String, String> params) throws Throwable{
+	public String generateNodeMsg(Map<String, String> params) throws Throwable {
 		StringBuffer res = new StringBuffer();
 		try {
-			int port;					//-po
-			String hostname; 			//-ho
-			String qmgrName; 			//-qm
-			String userId; 				//-um
-			String password; 			//-ps
-			String channelName; 		//-ch
-			String queueName; 			//-qu
-			String module;				//-mo
-			int generateCount = 1000; 	//-ge
-			int commitCount = 100;		//-co
-			String data;				//-da
-			String traceMsgCreator;     //-tr
+			int port; // -po
+			String hostname; // -ho
+			String qmgrName; // -qm
+			String userId; // -um
+			String password; // -ps
+			String channelName; // -ch
+			String queueName; // -qu
+			String module; // -mo
+			int generateCount = 1000; // -ge
+			int commitCount = 100; // -co
+			String data; // -da
+			String traceMsgCreator; // -tr
 			TraceMsgCreator tmc = null;
-	
+
 			if (checkParams(params)) {
 				qmgrName = params.get("qmgrName");
 				userId = params.get("userId");
@@ -153,39 +149,38 @@ public class GenerateTraceMsgService {
 				} catch (NumberFormatException e) {
 					port = 1414;
 				}
-				 
+
 				try {
 					generateCount = Integer.parseInt(params.get("generateCount"));
 				} catch (NumberFormatException e) {
 					generateCount = 1000;
 				}
-				
+
 				try {
 					commitCount = Integer.parseInt(params.get("commitCount"));
 				} catch (NumberFormatException e) {
 					commitCount = 100;
 				}
-				
+
 				traceMsgCreator = params.get("traceMsgCreator");
-				if(traceMsgCreator != null) {
+				if (traceMsgCreator != null) {
 					Class clazz = Class.forName(traceMsgCreator);
-					tmc = (TraceMsgCreator)clazz.newInstance(); 
+					tmc = (TraceMsgCreator) clazz.newInstance();
 				}
-				
-			} else { 
+
+			} else {
 				throw new IllegalArgumentException();
-			}	
-			 
-			
-			TraceMsgGenerator tmg = new TraceMsgGenerator(hostname, port, qmgrName, userId, password, channelName, queueName, module, generateCount, commitCount, data, tmc, cacheManager.getInterfaceCache());
-			 
-			
+			}
+
+			TraceMsgGenerator tmg = new TraceMsgGenerator(hostname, port, qmgrName, userId, password, channelName,
+					queueName, module, generateCount, commitCount, data, tmc, cacheManager.getInterfaceCache());
+
 			tmg.initialize();
-			 
+
 			tmg.generateNodeMsg(params);
-			
+
 			return res.append("ok").toString();
-		}catch (Throwable t) {
+		} catch (Throwable t) {
 			logger.info("------------------------------------------");
 			logger.info("- fail to generatd msgs.");
 			logger.info("------------------------------------------");
@@ -194,33 +189,43 @@ public class GenerateTraceMsgService {
 		}
 	}
 
-	
-	
-
 	/**
 	 * @return
 	 */
 	public static boolean checkParams(Map<String, String> params) {
 
-		boolean ok = params.containsKey("port") && 
-					 params.containsKey("hostname") && 
-					 params.containsKey("qmgrName") && 
-					 params.containsKey("channelName") && 
-					 params.containsKey("queueName") &&
-					 params.containsKey("module") &&
-					 params.containsKey("generateCount") &&
-					 params.containsKey("commitCount") &&
-					 params.containsKey("data") ;
-		
-		
-		
+		boolean ok = params.containsKey("port") &&
+				params.containsKey("hostname") &&
+				params.containsKey("qmgrName") &&
+				params.containsKey("channelName") &&
+				params.containsKey("queueName") &&
+				params.containsKey("module") &&
+				params.containsKey("generateCount") &&
+				params.containsKey("commitCount") &&
+				params.containsKey("data");
+
 		if (ok) {
-			try { Integer.parseInt((String) params.get("port")); } catch (NumberFormatException e) { ok = false; System.out.println("-po 파라미터는 숫자여야 합니다.");}
-			try { Integer.parseInt((String) params.get("generateCount")); } catch (NumberFormatException e) { ok = false; System.out.println("-ge 파라미터는 숫자여야 합니다.");}
-			try { Integer.parseInt((String) params.get("commitCount")); } catch (NumberFormatException e) { ok = false; System.out.println("-co 파라미터는 숫자여야 합니다.");}
+			try {
+				Integer.parseInt((String) params.get("port"));
+			} catch (NumberFormatException e) {
+				ok = false;
+				System.out.println("-po 파라미터는 숫자여야 합니다.");
+			}
+			try {
+				Integer.parseInt((String) params.get("generateCount"));
+			} catch (NumberFormatException e) {
+				ok = false;
+				System.out.println("-ge 파라미터는 숫자여야 합니다.");
+			}
+			try {
+				Integer.parseInt((String) params.get("commitCount"));
+			} catch (NumberFormatException e) {
+				ok = false;
+				System.out.println("-co 파라미터는 숫자여야 합니다.");
+			}
 		}
 
 		return ok;
 	}
-	
+
 }
